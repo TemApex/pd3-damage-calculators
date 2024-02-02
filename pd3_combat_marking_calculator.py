637a31cea02e7e55560f91254f85e459ef55b51a
Ema Pex <emapex12@gmail.com>
2024-02-02 21:06:18 +0000

    Add files via upload

---
+++ pd3_combat_marking_calculator.py
@@ -0,0 +1,93 @@
+#Define enemy data
+enemies = [
+    #[hp, armour, armourThreshold, Name]
+    (150, 70, 0.5, "Light Swat"),
+    (150, 170, 0.5, "Heavy Swat"),
+    (150, 140, 0.5, "Specials"),
+    (150, 0, 0, "Cloaker"),
+    (160, 180, 1, "Shield")
+    ]
+
+
+# Functions
+def damageCalc(shots, HP, AP, APMult, damage, critMult):
+    while True:
+        if (HP <= 0):
+            break
+        elif (AP <= 0):
+            HP -= damage * critMult
+            #print ('Shot {}. HP: {}'.format(shots,HP))
+        elif (AP > 0):
+            AP -= damage
+            HP -= APMult * damage * critMult
+            #print('Shot {}. AP: {}. HP: {}'.format(shots,AP,HP))
+        shots += 1
+    return shots
+        
+def NoEdge(HP, AP, APThreshold, enemy):
+    
+    shots = 0
+    APMult = APen - APThreshold
+    APMult = max([0, APMult])
+
+    shots = damageCalc(shots, HP, AP, APMult, damage, critMult)
+    
+    print ("{} killed without Edge in {} shots.".format(enemy,shots))
+
+def Edge(HP, AP, APThreshold, enemy):
+    
+    shots = 0
+    APMult = APen - APThreshold
+    APMult = max([0, APMult])
+        
+    shots = damageCalc(shots, HP, AP, APMult, damage * 1.1, critMult)
+    
+    print ("{} killed with Edge in {} shots.".format(enemy,shots))
+
+def FaceToFace(HP, AP, APThreshold, enemy):
+    shots = 0
+    APMult = APen - APThreshold
+    APMult = max([0, APMult])
+    
+    shots = damageCalc(shots, HP, AP, APMult, damage * 1.3, critMult)
+    
+    print ("{} killed with Combat Marking in {} shots.".format(enemy,shots))
+
+def CuttingShot(HP, AP, APThreshold, enemy):
+    shots = 0
+    APMult = APen - APThreshold + 0.1
+    APMult = max([0, APMult])
+    
+    shots = damageCalc(shots, HP, AP, APMult, damage * 1.1, critMult)
+    
+    print ("{} killed with Cutting Shot in {} shots.".format(enemy,shots))
+
+def CuttingFace(HP, AP, APThreshold, enemy):
+    shots = 0
+    APMult = APen - APThreshold + 0.1
+    APMult = max([0, APMult])
+    
+    shots = damageCalc(shots, HP, AP, APMult, damage * 1.3, critMult)
+    
+    print ("{} killed with Cutting Shot + Combat Marking in {} shots.".format(enemy,shots))
+
+while True:
+    print("-----------------------------------------")
+    try:
+        
+        #Gather weapon data
+        damage = float(input('Weapon damage: '))
+        critMult = float(input('Crit multiplier: '))
+        APen = float(input('AP Value: '))
+        print("")
+        
+        for enemy in enemies:
+            NoEdge(*enemy)
+            Edge(*enemy)
+            FaceToFace(*enemy)
+            CuttingShot(*enemy)
+            CuttingFace(*enemy)
+            print("")
+
+    except Exception as e:
+        print(e)
